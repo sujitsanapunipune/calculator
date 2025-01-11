70da4c2ed225b7ef32b03bdf56d3c422036eeab9
sujitsanapunipune <isujitsanap@gmail.com>
2025-01-11 17:44:36 +0530

    Multiplication Feater is Added Successfully

--- calculator1.py
+++ calculator1.py
@@ -10,15 +10,20 @@ def subtract(x, y):
     diff = x - y
     return print(f"Difference of {x} and {y} is {diff}")
 
+def Multiplication(x, y):
+    mult = x * y
+    return print(f"Multiplication of {x} and {y} is {mult}")
+
 # Main function
 def calculator():
     print("Simple Calculator. (My First Programme on GIT HUB)")
     print("Choose operation You want to Perform:")
     print("1. Addition")
     print("2. Subtraction")
+    print("3. Multiplication")
 
     # Take user input for operation
-    choice = input("Enter choice (1/2): ")
+    choice = input("Enter choice (1 Or 2 OR 3): ")
 
     # Take user input for numbers
     num1 = float(input("Enter first number: "))
@@ -29,8 +34,10 @@ def calculator():
         add(num1, num2)
     elif choice == '2':
         subtract(num1, num2)
+    elif choice == "3":
+        Multiplication(num1, num2)
     else:
-        print("Invalid input. Please select a valid operation (1 OR 2).")
+        print("Invalid input. Please select a valid operation (1 OR 2 Or 3).")
 
 # Run the calculator
 calculator()
